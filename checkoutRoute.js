bb6bd2163e45715c96826c2a838af7d09471ac1b
AbhishekP06 <webeyes02@gmail.com>
2025-09-11 23:03:10 +0530

    added frontend url

--- checkoutRoute.js
+++ checkoutRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
 router.post('/create-checkout-session', async (req, res) => {
     try {
