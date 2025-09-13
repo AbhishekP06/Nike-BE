021d436894af6cc97a67e93837316f82d7625257
AbhishekP06 <webeyes02@gmail.com>
2025-09-13 13:14:49 +0530

    updated cors

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express = require('express'); // Importing express
 const app = express(); // Creating an express app
 const cors = require("cors");
 
+const allowedOrigins = [
+    'https://nike-fe-bice.vercel.app',
+    'http://localhost:5173',
+    'http://localhost:3000'
+];
 app.use(cors({
-    origin: "*", // Allow all origins (not recommended for production)
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-  }));
+    origin: function (origin, callback) {
+        // Allow requests with no origin (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            console.log('CORS blocked origin:', origin);
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+}));
 
 //body parser
 app.use(express.json());
@@ -16,7 +32,7 @@ const product = require('./productRoute');
 const shoeType = require('./shoeRoute');
 const user = require('./userRoute');
 const checkoutRoute = require('./checkoutRoute');
-        
+
 connectDB();
 
 // Create a route that sends a response when visiting the homepage
@@ -24,7 +40,7 @@ connectDB();
 app.use('/products', product);
 app.use('/spotlight', shoeType);
 app.use('/user', user);
-app.use('/payment', checkoutRoute);  
+app.use('/payment', checkoutRoute);
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello, Shoe Server!</h1>');
