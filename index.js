44beeaf7968d8089e3309fd1d4a3aa45cdc5b297
AbhishekP06 <webeyes02@gmail.com>
2025-09-13 13:26:36 +0530

    updated cors

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ const shoeType = require('./shoeRoute');
 const user = require('./userRoute');
 const checkoutRoute = require('./checkoutRoute');
 
+// Debug environment variables
+console.log('Environment check:');
+console.log('MONGO_URI exists:', !!process.env.MONGO_URI);
+console.log('JWT_SECRET exists:', !!process.env.JWT_SECRET);
+console.log('STRIPE_SECRET_KEY exists:', !!process.env.STRIPE_SECRET_KEY);
+console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
+
 connectDB();
 
 // Create a route that sends a response when visiting the homepage
