05155250f3e8611500919e377310372db8e64763
AbhishekP06 <webeyes02@gmail.com>
2025-09-13 13:22:14 +0530

    updated cors

--- index.js
+++ index.js
@@ -2,26 +2,11 @@ const express = require('express'); // Importing express
 const app = express(); // Creating an express app
 const cors = require("cors");
 
-const allowedOrigins = [
-    'https://nike-fe-bice.vercel.app',
-    'http://localhost:5173',
-    'http://localhost:3000'
-];
 app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            console.log('CORS blocked origin:', origin);
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
+    credentials: false
 }));
 
 //body parser
